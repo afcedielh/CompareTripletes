06f17f09574697ed38e215b895a63a48e07cff3e
afcedielh <38447057+afcedielh@users.noreply.github.com>
2023-03-17 17:32:28 -0500

    Agregar archivos de proyecto.

---
+++ TestTiles/Domino.cs
@@ -0,0 +1,128 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+
+//class DominoTile
+//{
+//    public int SideA { get; }
+//    public int SideB { get; }
+
+//    public DominoTile(int sideA, int sideB)
+//    {
+//        SideA = sideA;
+//        SideB = sideB;
+//    }
+
+//    public override string ToString() => $"[{SideA}|{SideB}]";
+//}
+
+//class DominoChain
+//{
+//    private readonly List<DominoTile> _tiles;
+
+//    public DominoChain(IEnumerable<DominoTile> tiles)
+//    {
+//        _tiles = tiles.ToList();
+//    }
+
+//    public DominoChain(IEnumerable<int> sides)
+//    {
+//        if (sides.Count() % 2 != 0)
+//            throw new ArgumentException("El número de lados no es par.");
+
+//        _tiles = new List<DominoTile>();
+
+//        for (int i = 0; i < sides.Count(); i += 2)
+//        {
+//            _tiles.Add(new DominoTile(sides.ElementAt(i), sides.ElementAt(i + 1)));
+//        }
+//    }
+
+//    public bool IsValid()
+//    {
+//        if (_tiles.Count == 0)
+//            return true;
+
+//        if (_tiles.First().SideA != _tiles.Last().SideB)
+//            return false;
+
+//        for (int i = 1; i < _tiles.Count; i++)
+//        {
+//            if (_tiles[i - 1].SideB != _tiles[i].SideA)
+//                return false;
+//        }
+
+//        return true;
+//    }
+
+//    public void Sort()
+//    {
+//        if (IsValid())
+//            return;
+
+//        List<List<DominoTile>> permutations = GetPermutations(_tiles);
+
+//        foreach (List<DominoTile> permutation in permutations)
+//        {
+//            DominoChain chain = new DominoChain(permutation);
+
+//            if (chain.IsValid())
+//            {
+//                _tiles.Clear();
+//                _tiles.AddRange(permutation);
+//                return;
+//            }
+//        }
+//    }
+
+//    private List<List<DominoTile>> GetPermutations(List<DominoTile> tiles)
+//    {
+//        if (tiles.Count == 1)
+//            return new List<List<DominoTile>> { tiles };
+
+//        List<List<DominoTile>> permutations = new List<List<DominoTile>>();
+
+//        for (int i = 0; i < tiles.Count; i++)
+//        {
+//            DominoTile tile = tiles[i];
+
+//            List<DominoTile> remainingTiles = new List<DominoTile>(tiles);
+//            remainingTiles.RemoveAt(i);
+
+//            List<List<DominoTile>> subPermutations = GetPermutations(remainingTiles);
+
+//            foreach (List<DominoTile> subPermutation in subPermutations)
+//            {
+//                subPermutation.Insert(0, tile);
+//                permutations.Add(subPermutation);
+//            }
+//        }
+
+//        return permutations;
+//    }
+
+//    public override string ToString() => string.Join(" ", _tiles);
+//}
+
+////class Program
+////{
+////    static void Main(string[] args)
+////    {
+////        List<DominoTile> tiles1 = new List<DominoTile>
+////        {
+////            new DominoTile(2, 1),
+////            new DominoTile(2, 3),
+////            new DominoTile(1, 3)
+////        };
+
+////        DominoChain chain1 = new DominoChain(tiles1);
+////        chain1.Sort();
+////        Console.WriteLine(chain1);
+
+////        List<int> sides2 = new List<int> { 1, 2, 4, 1, 2, 3 };
+
+////        DominoChain chain2 = new DominoChain(sides2);
+////        chain2.Sort();
+////        Console.WriteLine(chain2);
+////    }
+////}
---
+++ TestTiles/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'compareTriplets' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY a
+     *  2. INTEGER_ARRAY b
+     */
+
+    public static List<int> compareTriplets(List<int> a, List<int> b)
+    {
+        List<int> ret = new List<int>(){
+            0,0
+        };
+        for (int i = 0; i <= 2; i++)
+        {
+            ret[0] = ret[0] + (a[i] > b[i] ? 1 : 0);
+            ret[1] = ret[1] + (b[i] > a[i] ? 1 : 0);
+        }
+        return ret;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        List<int> a = new List<int>() { 5, 6, 7 };
+
+        List<int> b = new List<int>() { 3, 6, 10 };
+
+        List<int> result = Result.compareTriplets(a, b);
+
+        //textWriter.WriteLine(String.Join(" ", result));
+
+        //textWriter.Flush();
+        //textWriter.Close();
+    }
+}
